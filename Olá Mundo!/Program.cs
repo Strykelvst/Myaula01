34687d320eb8b7bf4c987afe0714cb9fa8ee86b1
Strykelvst <45168392+Strykelvst@users.noreply.github.com>
2019-03-01 15:59:37 +0000

    Add project files.

---
+++ Olá Mundo!/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Olá_Mundo_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
